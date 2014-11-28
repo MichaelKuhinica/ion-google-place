fc96ae1af4cb3a512f2335421b3fc87c84512a27
Michael M Kuhinica <taverneiro@gmail.com>
2014-11-28 18:54:43 -0200

    Powered by google local

--- ion-google-place.js
+++ ion-google-place.js
@@ -39,6 +39,9 @@ angular.module('ion-google-place', [])
                   '<ion-item ng-repeat="location in locations" type="item-text-wrap" ng-click="selectLocation(location)">',
                   '{{location.description}}',
                   '</ion-item>',
+                  '<ion-item ng-show="locations.length > 0">',
+                  '<div class="disclamer"><img ng-src="/img/powered-by-google-on-white.png"></div>',
+                  '</ion-item>',
                   '</ion-list>',
                   '</ion-content>',
                   '</div>'
